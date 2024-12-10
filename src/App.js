afc539374a209e8a2ba0b12102abd3e93212bcbb
Ernie Tsogt <terdemt@yahoo.com>
2024-12-10 12:47:52 +0100

    first frontend version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import Borrow from './pages/Borrow';
+import Return from './pages/Return';
+import AddBook from './components/AddBook';
+import EditBook from './components/EditBook';
+import BorrowRecords from './components/BorrowRecords';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/borrow" element={<Borrow />} />
+          <Route path="/return" element={<Return />} />
+          <Route path="/books/add" element={<AddBook />} />
+          <Route path="/books/edit" element={<EditBook />} />
+          <Route path="/borrow-records" element={<BorrowRecords />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
